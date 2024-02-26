1474eec76b91e0a8a59ecaf76ecc75e5688c2ec6
Reda Chehabi <loxgaming99@gmail.com>
2024-02-26 09:06:32 +0100

    Done

--- mot-de-passe.py
+++ mot-de-passe.py
@@ -28,28 +28,28 @@ def validate():
     number = "^(06|07)[0-9]{8}$"
     email = "^[a-zA-Z]+[a-zA-Z0-9_.-]*@[A-Za-z0-9_.-]+\.[a-zA-Z]{2,}$"
     if re.search(name, Z1.get()) and re.search(number, Z2.get()) and re.search(email, Z3.get()):
-        L4 = tkinter.Label(text="Valide info", bg="purple", fg="green")
+        L4 = tkinter.Label(text="Valid info", bg="purple", fg="green")
         L4.place(x=180, y=220)
     else:
-        L5 = tkinter.Label(text="Invalide info", bg="purple", fg="red")
+        L5 = tkinter.Label(text="Invalid info", bg="purple", fg="red")
         L5.place(x=180, y=220)
     if re.search(name, Z1.get()):
-        l1 = tkinter.Label(text="Valide name", bg="purple", fg="green")
+        l1 = tkinter.Label(text="Valid name", bg="purple", fg="green")
         l1.place(x=380,y=30)
     else:
-        l2 = tkinter.Label(text="Invalide name", bg="purple", fg="red")
+        l2 = tkinter.Label(text="Invalid name", bg="purple", fg="red")
         l2.place(x=380,y=30)
     if re.search(number, Z2.get()):
-        l3 = tkinter.Label(text="Valide Number", bg="purple", fg="green")
+        l3 = tkinter.Label(text="Valid Number", bg="purple", fg="green")
         l3.place(x=380,y=75)
     else:
-        l4 = tkinter.Label(text="Invalide Number", bg="purple", fg="red")
+        l4 = tkinter.Label(text="Invalid Number", bg="purple", fg="red")
         l4.place(x=380,y=75)
     if re.search(email, Z3.get()):
-        l5 = tkinter.Label(text="Valide Email", bg="purple", fg="green")
+        l5 = tkinter.Label(text="Valid Email", bg="purple", fg="green")
         l5.place(x=380,y=120)
     else:
-        l6 = tkinter.Label(text="Invalide Email", bg="purple", fg="red")
+        l6 = tkinter.Label(text="Invalid Email", bg="purple", fg="red")
         l6.place(x=380,y=120)
 
 """
@@ -65,9 +65,9 @@ def validate():
         L5.place(x=180,y=220)
 """
 but1 = tkinter.Button(text="Effacer",command=clear)
-but1.place(x=180,y=160)
+but1.place(x=230,y=160)
 but2 = tkinter.Button(text="Valider",command=validate)
-but2.place(x=280,y=160)
+but2.place(x=320,y=160)
 
 
 window.mainloop()
